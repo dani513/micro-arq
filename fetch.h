ac8b387972e018c6d7e78c57a973256d9989a64b
yisusv <58369020+yisusv@users.noreply.github.com>
2020-01-20 00:00:28 -0400

    Update fetch.h

--- fetch.h
+++ fetch.h
@@ -11,33 +11,33 @@ using namespace std;
 
 SC_MODULE (fetch)
 {
-	ifstream MI;
-	sc_in <bool> clk;
-	sc_out < sc_uint<INSTRUCTION_SIZE> > instruction_in;
-	
-	void Fetch()
-	{
-		string line;
-		getline(MI,line);
-		sc_uint <INSTRUCTION_SIZE> temp;
-		
-		for (int i = 0; i < INSTRUCTION_SIZE; ++i) 
-			temp[i] = line[INSTRUCTION_SIZE-(i+1)]-'0';
-		
-		instruction_in.write(temp);
-	}
-	
-	SC_CTOR (fetch)
-	{
-		MI.open(MI_filename);
-		SC_METHOD(Fetch);
-		sensitive << clk.neg();
-	}
-	
-	~fetch()
-	{
-		MI.close();
-	}
+  ifstream MI;
+  sc_in <bool> clk;
+  sc_out < sc_uint<INSTRUCTION_SIZE> > instruction_in;
+  
+  void Fetch()
+  {
+    string line;
+    getline(MI,line);
+    sc_uint <INSTRUCTION_SIZE> temp;
+    
+    for (int i = 0; i < INSTRUCTION_SIZE; ++i) 
+      temp[i] = line[INSTRUCTION_SIZE-(i+1)]-'0';
+    
+    instruction_in.write(temp);
+  }
+  
+  SC_CTOR (fetch)
+  {
+    MI.open(MI_filename);
+    SC_METHOD(Fetch);
+    sensitive << clk.neg();
+  }
+  
+  ~fetch()
+  {
+    MI.close();
+  }
 };
 
 #endif
