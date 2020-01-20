0eb258fc79304b448acc4920f4c00707470fe4a9
yisusv <58369020+yisusv@users.noreply.github.com>
2020-01-19 23:45:28 -0400

    Update pipe.h

--- pipe.h
+++ pipe.h
@@ -10,35 +10,35 @@ using namespace std;
 class Pipe : public sc_module
 {
 public:
-	sc_in < sc_uint<INSTRUCTION_SIZE> > pi_in;
-	sc_out < sc_uint<INSTRUCTION_SIZE> > pi_out;
-	sc_in <bool> clk_in;
+  sc_in < sc_uint<INSTRUCTION_SIZE> > pi_in;
+  sc_out < sc_uint<INSTRUCTION_SIZE> > pi_out;
+  sc_in <bool> clk_in;
+  
+  SC_CTOR(Pipe)
+  {
+    this->data = 0;
+    SC_METHOD(write_operation);
+    sensitive << clk_in << pi_in;
+    SC_METHOD(read_operation);
+    sensitive << clk_in;
+  }
+  
+  ~Pipe(){}
 
-	SC_CTOR(Pipe)
-	{
-		this->data = 0;
-		SC_METHOD(write_operation);
-		sensitive << clk_in << pi_in;
-		SC_METHOD(read_operation);
-		sensitive << clk_in;
-	}
-
-	~Pipe(){}
-	
 private:
-	sc_uint <INSTRUCTION_SIZE> data;
-
-	void write_operation()
-	{
-		if(!clk_in.read() and pi_in.read())
-			data = pi_in;
-	}
-
-	void read_operation()
-	{
-		if (clk_in.read())
-			pi_out.write(data);
-	}
+  sc_uint <INSTRUCTION_SIZE> data;
+  
+  void write_operation()
+  {
+    if(!clk_in.read() and pi_in.read())
+      data = pi_in;
+  }
+  
+  void read_operation()
+  {
+    if (clk_in.read())
+      pi_out.write(data);
+  }
 };
 
 #endif
